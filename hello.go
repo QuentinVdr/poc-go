2f78f2a6f8e59ed296a0660b8ab377f0589e1e11
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-10-22 12:25:27 +0200

    ✨ Update go.mod and hello.go to add new dependencies and implement a RESTful API endpoint

--- hello.go
+++ hello.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 
 	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println("Optimization truth : ", quote.Opt())
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Optimization truth: %s", quote.Opt()),
+		})
+	})
+
+	router.Run(":8080")
 }
